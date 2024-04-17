dc032be132ed09c6bf93655777d8526dc9ec8ece
birudevr2000 <166617370+birudevr2000@users.noreply.github.com>
2024-04-17 17:11:24 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int s,n;
+      cout<<"enter the size of array:"<<endl;
+      cin>>s; 
+      cout<<"number of elements in array:"<<endl;
+      cin>>n;
+      for()
+      
+}
